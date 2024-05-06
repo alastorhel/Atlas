91a7b38269f921bcad60f5edc2f8ff91a70150b3
Eduardo <dukahenrick06@gmail.com>
2024-05-06 07:45:51 -0300

    aonde tudo começou

---
+++ Atlas/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Atlas;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Atlas/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Atlas;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
